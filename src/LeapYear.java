969217c39e25c2a3acfc8f79ad50d389a76fa519
Sally Resch <sallyresch27@gmail.com>
2022-09-05 15:40:15 +0200

    Initial commit

---
+++ src/LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if (!(year >= 1 && year <= 9999)){
+            return false;
+        }
+        else if(year >= 1 && year <= 9999){
+            if(year% 4==0){
+                if(year % 100==0){
+                    if(year % 400==0){
+                    }return false;
+                }return true;
+            }return false;
+        }return true;
+    }
+}
